1965705c2b3e214fcf389ae322a398fd20bf098e
HansM7 <melchorhans5@gmail.com>
2023-11-05 17:26:26 -0500

    correction scss

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
