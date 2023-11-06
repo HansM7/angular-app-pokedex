fa71b3b9e56fce48a130114a2094e90f8300fce8
HansM7 <melchorhans5@gmail.com>
2023-11-06 07:17:07 -0500

    add api external

--- src/app/environments/api.ts
+++ src/app/environments/api.ts
@@ -1,3 +1,5 @@
 export const api_url: string = 'https://pokeapi.co/api/v2/pokemon';
 export const api_url_image: string =
   'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+
+export const api_backend = 'https://pokedex-api-dev-rssj.3.us-1.fl0.io/api/';
